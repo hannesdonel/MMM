d3b1ad693f47c997224f81760aebaefe5e3db25b
hannesdonel <mail@hannesdonel.de>
2021-08-04 09:36:10 +0200

    Update allowedOrigins.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const Directors = Models.Director;
 const Users = Models.User;
 
 // Trusted domain
-const allowedOrigins = ['http://localhost:8080'];
+const allowedOrigins = ['http://localhost:8080', 'https://herokuapp.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
