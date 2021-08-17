172e7fa6456617636c1d7099e722cd6d9d799b55
hannesdonel <mail@hannesdonel.de>
2021-08-17 13:11:31 +0200

    Require cors earlier.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-const cors = require('cors');
 
 app.use(morgan('common'));
 
