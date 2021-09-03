8fed22027ebc632bb4de6b0afe8dd17cfc97c832
hannesdonel <mail@hannesdonel.de>
2021-09-03 11:31:18 +0200

    Update user: ID comparison was just the wrong way around. Solved that.

--- users/users-services.js
+++ users/users-services.js
@@ -81,7 +81,7 @@ const UsersServices = {
     try {
       const anotherUser = await Users.findOne({ user_name: req.body.user_name });
       const user = await Users.findOne({ _id: req.params._id });
-      if (Object.keys(req.body).length > 0 && anotherUser._id !== user._id) {
+      if (Object.keys(req.body).length > 0 && anotherUser._id === user._id) {
         return { success: false, statusCode: 404, message: `${req.body.user_name} already exists. Please choose another username.` };
       } if (Object.keys(req.body).length > 0 && user) {
         const updateObject = {};
