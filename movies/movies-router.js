30d524dcc7249a02d7c06a2109e90d01c72a708d
hannesdonel <mail@hannesdonel.de>
2021-11-18 10:09:10 +0100

    Test fixing async router functions.

--- movies/movies-router.js
+++ movies/movies-router.js
@@ -9,29 +9,29 @@ MoviesRouter
   .get('/:title', passport.authenticate('jwt', { session: false }), async (req, res) => {
     try {
       const result = await MoviesServices.get_movie_by_title(req);
-      console.log(result);
-      // if (!result.success && result.statusCode === 404) {
-      //   res.status(404).send(result.message);
-      // } else if (!result.success && result.statusCode === 500) {
-      //   res.status(500).send(result.error);
-      // } else {
-      //   res.status(200).send(result.movie);
-      // }
+      if (!result.success && result.statusCode === 404) {
+        res.status(404).send(result.message);
+      } else if (!result.success && result.statusCode === 500) {
+        res.status(500).send(result.error);
+      } else {
+        res.status(200).send(result.movie);
+      }
     } catch (error) {
       res.status(500).send(error);
     }
   })
 // Get all movies and movies by genre and/or actor
   .get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    const result = await MoviesServices.get_movies_by_genre_actor(req);
     try {
-      if (!result.success && result.statusCode === 404) {
-        res.status(404).send(result.message);
-      } else if (!result.success && result.statusCode === 500) {
-        res.status(500).send(result.error);
-      } else {
-        res.status(200).send(result.movies);
-      }
+      const result = await MoviesServices.get_movies_by_genre_actor(req);
+      console.log(result);
+      // if (!result.success && result.statusCode === 404) {
+      //   res.status(404).send(result.message);
+      // } else if (!result.success && result.statusCode === 500) {
+      //   res.status(500).send(result.error);
+      // } else {
+      //   res.status(200).send(result.movies);
+      // }
     } catch (error) {
       res.status(500).send(error);
     }
